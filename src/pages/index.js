bf09ce47778d5e4eca38411c7ccd2e6c4cbf5e5a
Jeff Pittman <geojeff@geojeff.org>
2020-02-23 07:57:32 -0600

    Added a Grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 // - import { Link } from "gatsby"
 // +
 import Link from "../components/link"
-import { Box, Heading, Paragraph } from "grommet"
+import { Box, Grid, Heading, Paragraph } from "grommet"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -14,6 +14,36 @@ const IndexPage = () => (
     <Heading>Hi people</Heading>
     <Paragraph>Welcome to your new Gatsby site.</Paragraph>
     <Paragraph>Now go build something great.</Paragraph>
+            <Grid align='stretch' pad={{ horizontal: 'large' }}
+              rows={['flex']}
+              columns={['flex', 'flex', 'flex', 'flex']}
+              gap="small"
+              margin="small"
+              areas={[
+                { name: 'one', start: [0, 0], end: [0, 0] },
+                { name: 'two', start: [1, 0], end: [1, 0] },
+                { name: 'three', start: [2, 0], end: [2, 0] },
+                { name: 'four', start: [3, 0], end: [3, 0] },
+              ]}
+            >
+              <Box flex gridArea="one" background="light-2">
+                <Heading margin="small" size="small">One</Heading>
+                <Paragraph margin="small">This is text. This is text. This is text. This is text. This is text. This is text. This is text. This is text. </Paragraph>
+                <Paragraph margin="small">This is text. This is text. This is text. This is text. This is text. This is text. This is text. This is text. </Paragraph>
+              </Box>
+              <Box flex gridArea="two" background="light-2">
+                <Heading margin="small" size="small">Two</Heading>
+                <Paragraph margin="small">This is text. This is text. This is text. This is text. This is text. This is text. This is text. This is text. </Paragraph>
+              </Box>
+              <Box flex gridArea="three" background="light-2">
+                <Heading margin="small" size="small">Three</Heading>
+                <Paragraph margin="small">This is text. This is text. This is text. This is text. This is text. This is text. This is text. This is text. </Paragraph>
+              </Box>
+              <Box flex gridArea="four" background="light-2">
+                <Heading margin="small" size="small">Four</Heading>
+                <Paragraph margin="small">This is text. This is text. This is text. This is text. This is text. This is text. This is text. This is text. </Paragraph>
+              </Box>
+            </Grid>
     <Box width={{ max: "300px" }} pad="small">
       <Image />
     </Box>
